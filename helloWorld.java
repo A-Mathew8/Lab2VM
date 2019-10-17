df01bb744e048e31d7949c435fdc3aa2e26ce3ed
Ubuntu <AMathew8@lab2VM.p2xwy3qreazupdvl3k20zttfzh.bx.internal.cloudapp.net>
2019-10-17 13:51:38 +0000

    first Commit

---
+++ helloWorld.java
@@ -0,0 +1,15 @@
+/* This is a simple Java program. 
+   FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+        System.out.println("Test Line 1"); 
+        System.out.println("Test Line 2"); 
+        System.out.println("Test Line 3"); 
+    } 
+}
+
